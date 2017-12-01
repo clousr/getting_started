45ae738ab5c650a15dc0f7aeb4d39960305d052f
Ryan Clous <rclous.zteam@zuora.com>
2017-12-01 13:43:43 -0500

    added show function

--- blog/app/controllers/articles_controller.rb
+++ blog/app/controllers/articles_controller.rb
@@ -1,6 +1,6 @@
 class ArticlesController < ApplicationController
-	def new
-	end
+def new
+end
 
 def create
   @article = Article.new(article_params)
@@ -8,7 +8,11 @@ def create
   @article.save
   redirect_to @article
 end
- 
+
+def show
+ @article = Article.find(params[:id])
+end
+
 private
   def article_params
     params.require(:article).permit(:title, :text)
