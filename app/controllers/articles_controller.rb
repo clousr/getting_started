7bdf1ab38a79ab07ea205f4a0afb3b050526bc10
Ryan Clous <rclous.zteam@zuora.com>
2017-12-03 13:20:38 -0500

    added article editing

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -16,6 +16,21 @@ class ArticlesController < ApplicationController
 	  end
 	end
 
+	def update
+		@article = Article.find(params[:id])
+	 
+		if
+			@article.update(article_params)
+		    redirect_to @article
+		else
+			render 'edit'
+		end
+	end
+
+	def edit
+		@article = Article.find(params[:id])
+	end
+
 	def show
 	 @article = Article.find(params[:id])
 	end
